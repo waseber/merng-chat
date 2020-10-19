95b685391d58d513ee405c13ed1ee7efc1616ab7
waseber <yvesgerber@gmail.com>
2020-10-19 16:30:51 -0400

    adds user components

---
+++ graphql/resolvers/users.js
@@ -0,0 +1,40 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const {SECRET_KEY} = require('../../config');
+const User = require('../../models/User');
+
+module.exports = {
+    Mutation:{
+        async register(_, { registerInput : {username, email, password, confirmPassword}}, context, info){
+            // TODO Validate user data
+            // TODO make sure user doesn't already exist
+
+            const user = await User.findOne({username})
+            if(user){}
+            // hash password create an auth token
+            password = await bcrypt.hash(password, 12);
+
+            const newUser = new User({
+                email,
+                username,
+                password,
+                createdAt: new Date().toISOString()
+            })
+
+            const res = await newUser.save();
+
+            const token = jwt.sign({
+                id: res.id,
+                email: res.email,
+                username: res.username
+            }, SECRET_KEY, {expiresIn: '1h'});
+
+            return{
+                ...res._doc,
+                id: res._id,
+                token
+            }
+        }
+    }
+}
