cb7b99aaea86e78dee4d4fac1ba022a040575911
waseber <yvesgerber@gmail.com>
2020-10-26 15:12:43 -0400

    client connects to backend

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,41 @@
 import React from 'react';
+import {useQuery} from '@apollo/react-hooks';
+import gql from 'graphql-tag';
 
 function Home(){
+    const {loading, data} = useQuery(FETCH_POSTS_QUERY)
+
+    if(data){
+        console.log(data);
+    }
+
     return(
         <div>
             <h1>Home</h1>
         </div>
     )
 }
+
+const FETCH_POSTS_QUERY = gql`
+    {
+        getPosts {
+            id
+            body
+            createdAt
+            username
+            likeCount
+            likes{
+                username
+            }
+            commentCount
+            comments{
+                id
+                username
+                createdAt
+                body
+            }
+        }
+    }
+`
 
 export default Home;
